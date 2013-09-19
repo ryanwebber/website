b4ee43808cba62bf39b5e6e6e0cb03ddfd842cb5
ryanwebber <ryan551024@gmail.com>
2013-09-19 18:25:55 -0400

    Node fix for styling and requirments

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
@@ -6,8 +7,10 @@ app.get('/', function(request, response) {
         var html = fs.readFileSync('index.html').toString();
 	response.send(html);
 });
-
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/", express.static(__dirname + '/'));
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

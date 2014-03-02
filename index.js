b725bb0a8c04aec81089018db3045c7ba969d763
Ryan Webber <ryanwebber@Ryans-MacBook-Air.local>
2014-03-02 14:01:20 -0500

    New ui

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var express = require("express");
-var fs = require('fs');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-        var html = fs.readFileSync('index.html').toString();
-	response.send(html);
-});
-app.use("/css", express.static(__dirname + '/css'));
-app.use("/", express.static(__dirname + '/'));
+app.use("/", express.static(__dirname + '/www'));
+
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
